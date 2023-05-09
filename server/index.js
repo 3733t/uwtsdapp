ddb10a595d2556573011286a3b4b3f7f742df1ef
kizitoomoit <kizitoomoit9@gmail.com>
2023-05-09 11:59:03 +0300

    updated the cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 
 const PORT = process.env.PORT || 8080;
 
-app.use(cors())
+app.use(cors({origin: "https://utwsdapp1.azurewebsites.net"}));
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../client/build')));
